bbf19dca39f3d384d2ed0c8d9c27a81e88261fe6
René Rettig <rene@itambox.dev>
2026-08-10 11:16:11 +0200

    style(compliance): sort imports in custody RBAC suite

--- itambox/compliance/tests/test_custody_rbac.py
+++ itambox/compliance/tests/test_custody_rbac.py
@@ -11,7 +11,6 @@ from io import StringIO
 from unittest.mock import patch
 
 import pytest
-
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import Permission
 from django.contrib.contenttypes.models import ContentType
